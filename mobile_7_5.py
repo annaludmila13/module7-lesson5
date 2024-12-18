4af709810090a42be5e6ab47abd3a149be5b84b4
annaludmila13 <annaludmila10@gmail.com>
2024-12-18 21:51:59 +0300

    Add files via upload

---
+++ mobile_7_5.py
@@ -0,0 +1,28 @@
+import os
+import time
+
+# Указываем директорию для обхода
+directory = "."
+
+# Используем os.walk для обхода всех файлов в указанной директории
+for root, dirs, files in os.walk(directory):
+    # Обрабатываем каждый файл
+    for file in files:
+        # Формируем полный путь к файлу
+        filepath = os.path.join(root, file)
+
+        # Получаем время последнего изменения файла
+        filetime = os.path.getmtime(filepath)
+
+        # Преобразуем время в читаемый формат
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+
+        # Получаем размер файла
+        filesize = os.path.getsize(filepath)
+
+        # Получаем родительскую директорию файла
+        parent_dir = os.path.dirname(filepath)
+
+        # Выводим информацию о файле
+        print(
+            f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
